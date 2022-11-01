0b798de9f146f1feca5231e6271b5f75741318b6
petrostrak <pit.trak@gmail.com>
2022-11-01 12:00:59 +0200

    Test auth middleware

--- webapp/cmd/web/middleware_test.go
+++ webapp/cmd/web/middleware_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"net/http/httptest"
+	"simple-web-app/pkg/data"
 	"strings"
 	"testing"
 )
@@ -70,5 +71,40 @@ func Test_application_ipFromContext(t *testing.T) {
 	// perform the test
 	if !strings.EqualFold("whatever", ip) {
 		t.Error("wrong value returned from context")
+	}
+}
+
+func Test_App_auth(t *testing.T) {
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	var tests = []struct {
+		name   string
+		isAuth bool
+	}{
+		{"logged in", true},
+		{"not logged in", false},
+	}
+
+	for _, e := range tests {
+		handlerToTest := app.auth(nextHandler)
+		req := httptest.NewRequest("GET", "http://localhost:8080/user/profile", nil)
+		req = addContextAndSessionToRequest(req, app)
+
+		if e.isAuth {
+			app.Session.Put(req.Context(), "user", data.User{ID: 1})
+		}
+
+		rr := httptest.NewRecorder()
+		handlerToTest.ServeHTTP(rr, req)
+
+		if e.isAuth && rr.Code != http.StatusOK {
+			t.Errorf("%s: expected status code of 200 but got %d", e.name, rr.Code)
+		}
+
+		if !e.isAuth && rr.Code != http.StatusTemporaryRedirect {
+			t.Errorf("%s: expected status code 307, but got %d", e.name, rr.Code)
+		}
 	}
 }

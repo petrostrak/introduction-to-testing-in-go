6d27397bec6c20f0ba5dcb00970af83afeb087f8
petrostrak <pit.trak@gmail.com>
2022-11-02 11:46:15 +0200

    Update setup_test.go to use the test repo

--- webapp/cmd/web/setup_test.go
+++ webapp/cmd/web/setup_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 	"simple-web-app/repository/dbrepo"
 	"testing"
@@ -13,15 +12,7 @@ func TestMain(m *testing.M) {
 	pathToTemlpates = "./../../templates/"
 	app.Session = getSession()
 
-	app.DSN = "host=localhost port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5"
-
-	conn, err := app.connectToDB()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+	app.DB = &dbrepo.TestDBRepo{}
 
 	os.Exit(m.Run())
 }

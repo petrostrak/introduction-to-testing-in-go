b0db90c765e301bb2ca591a01b70c9fbe7436ea0
petrostrak <pit.trak@gmail.com>
2022-11-07 14:31:32 +0200

    rest api; Implement authenticate handler

--- webapp/cmd/api/api-handlers.go
+++ webapp/cmd/api/api-handlers.go
@@ -1,17 +1,52 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
 
+	"golang.org/x/crypto/bcrypt"
+)
+
+// Credentials is a type that we can unmarshal the json into.
+type Credentials struct {
+	Username string `json:"email"`
+	Password string `json:"password"`
+}
+
+// curl http://localhost:8090/auth -X POST -H "Content-Type: application/json" -d '{"email":"admin@example.com","password":"secret"}'
 func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+
 	// read a json payload
+	err := app.readJSON(w, r, &creds)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
 
 	// look up the user by email address
+	user, err := app.DB.GetUserByEmail(creds.Username)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
 
 	// check password
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(creds.Password))
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
 
 	// generate tokens
+	tokenPairs, err := app.generateTokenPair(user)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
 
 	// send token to user
+	_ = app.writeJSON(w, http.StatusOK, tokenPairs)
 }
 
 func (app *application) refresh(w http.ResponseWriter, r *http.Request) {

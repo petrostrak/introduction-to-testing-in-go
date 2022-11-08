bd62a0060d09088e49277c2cd8d91d3e4187a847
petrostrak <pit.trak@gmail.com>
2022-11-08 11:16:13 +0200

    rest api; setup application middleware enableCORS

---
+++ webapp/cmd/api/api-middleware.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+// enables CORS protection
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8090")
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, X-CSRF-Token, Authorization")
+			return
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}

392b7e9379985dd89ee144ace0a3bed887f78802
petrostrak <pit.trak@gmail.com>
2022-11-08 20:15:48 +0200

    SPA; Set refresh token cookie when authenticating

--- webapp/cmd/api/api-handlers.go
+++ webapp/cmd/api/api-handlers.go
@@ -50,6 +50,18 @@ func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	http.SetCookie(w, &http.Cookie{
+		Name:     "__Host-refresh_token",
+		Path:     "/",
+		Value:    tokenPairs.RefreshToken,
+		Expires:  time.Now().Add(refreshTokenExpiry),
+		MaxAge:   int(refreshTokenExpiry.Seconds()),
+		SameSite: http.SameSiteStrictMode,
+		Domain:   "localhost",
+		HttpOnly: true,
+		Secure:   true,
+	})
+
 	// send token to user
 	_ = app.writeJSON(w, http.StatusOK, tokenPairs)
 }

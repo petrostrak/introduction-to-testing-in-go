5ccfb23db4a67f3366bcf954d5c46000e68f4a7b
petrostrak <pit.trak@gmail.com>
2022-11-08 10:41:02 +0200

    rest api; Add cli program that generates tokens for testing

--- webapp/cmd/api/setup_test.go
+++ webapp/cmd/api/setup_test.go
@@ -6,7 +6,10 @@ import (
 	"testing"
 )
 
-var app application
+var (
+	app          application
+	expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiYXVkIjoiZXhhbXBsZS5jb20iLCJleHAiOjE2Njc1MzY3ODMsImlzcyI6ImV4YW1wbGUuY29tIiwibmFtZSI6IkpvaG4gRG9lIiwic3ViIjoiMSJ9.kiAYYa_vh9Gr8dqp-P3wg2969Rlwl9RmtxKC25Zpv6M"
+)
 
 func TestMain(m *testing.M) {
 	app.DB = &dbrepo.TestDBRepo{}

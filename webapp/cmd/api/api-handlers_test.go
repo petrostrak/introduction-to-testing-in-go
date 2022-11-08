6a56723154b080a7be80d32f8e1fa6285b6b76cf
petrostrak <pit.trak@gmail.com>
2022-11-08 14:27:15 +0200

    rest api; Test refresh tokens

--- webapp/cmd/api/api-handlers_test.go
+++ webapp/cmd/api/api-handlers_test.go
@@ -3,8 +3,11 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"simple-web-app/pkg/data"
 	"strings"
 	"testing"
+	"time"
 )
 
 func Test_app_authenticate(t *testing.T) {
@@ -31,5 +34,57 @@ func Test_app_authenticate(t *testing.T) {
 		if e.expectedStatusCode != rr.Code {
 			t.Errorf("%s: returned wrong status code, expected %d but got %d.", e.name, e.expectedStatusCode, rr.Code)
 		}
+	}
+}
+
+func Test_app_refresh(t *testing.T) {
+	var tests = []struct {
+		name               string
+		token              string
+		expectedStatusCode int
+		resetRefreshTime   bool
+	}{
+		{"valid", "", http.StatusOK, true},
+		{"valid but not yet ready to expire", "", http.StatusTooEarly, false},
+		{"expired", expiredToken, http.StatusBadRequest, false},
+	}
+
+	testUser := data.User{
+		ID:        1,
+		FirstName: "admin",
+		LastName:  "User",
+		Email:     "admin@example.com",
+	}
+
+	oldRefreshTime := refreshTokenExpiry
+
+	for _, e := range tests {
+		var tkn string
+		if e.token == "" {
+			if e.resetRefreshTime {
+				refreshTokenExpiry = time.Second
+			}
+			tokens, _ := app.generateTokenPair(&testUser)
+			tkn = tokens.RefreshToken
+		} else {
+			tkn = e.token
+		}
+
+		postedData := url.Values{
+			"refresh_token": {tkn},
+		}
+
+		req, _ := http.NewRequest("POST", "/refresh-token", strings.NewReader(postedData.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		rr := httptest.NewRecorder()
+
+		handler := http.HandlerFunc(app.refresh)
+		handler.ServeHTTP(rr, req)
+
+		if rr.Code != e.expectedStatusCode {
+			t.Errorf("%s: expected status of %d but got %d", e.name, e.expectedStatusCode, rr.Code)
+		}
+
+		refreshTokenExpiry = oldRefreshTime
 	}
 }
